85d5af610463b61624ac70d5f1afd4605a278a62
Jacques <48244899+Jacques-dev@users.noreply.github.com>
2023-02-26 12:14:15 +0100

    ménage

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Model_Prediction_Devops in a project::
-
-    import model_prediction_devops
--- model_prediction_devops/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Model_Prediction_Devops."""
-
-__author__ = """Noureddine"""
-__email__ = 'noureddine.oubenami@gmail.com'
-__version__ = '0.1.0'
